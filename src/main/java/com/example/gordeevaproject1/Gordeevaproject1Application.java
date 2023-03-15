c060209fe556c7a96458519e362e0a3220de6fc8
Gordeeva Olga <olja.gord00@gmail.com>
2023-03-15 14:33:19 +0500

    lab-1

---
+++ src/main/java/com/example/gordeevaproject1/Gordeevaproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.gordeevaproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gordeevaproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gordeevaproject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gordeevaproject1/Gordeevaproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gordeevaproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gordeevaproject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
